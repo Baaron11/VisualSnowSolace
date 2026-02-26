2d26f94e0a0931309069b7f4d2455628766e0306
Brandon Ramirez <brandonramirez77@yahoo.com>
2026-02-26 14:34:31 -0800

    Initial Commit

---
+++ Visual Snow Solace/Visual_Snow_SolaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Visual_Snow_SolaceApp.swift
+//  Visual Snow Solace
+//
+//  Created by Brandon Ramirez on 2/26/26.
+//
+
+import SwiftUI
+
+@main
+struct Visual_Snow_SolaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Visual Snow SolaceTests/Visual_Snow_SolaceTests.swift
@@ -0,0 +1,16 @@
+//
+//  Visual_Snow_SolaceTests.swift
+//  Visual Snow SolaceTests
+//
+//  Created by Brandon Ramirez on 2/26/26.
+//
+
+import Testing
+
+struct Visual_Snow_SolaceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
